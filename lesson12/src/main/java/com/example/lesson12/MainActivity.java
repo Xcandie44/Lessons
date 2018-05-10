74c909ec45c9c73c8098e46240e1a7f15c5abc8c
Xcandie44 <pechnikov.ivan@yandex.by>
2018-05-10 17:51:00 +0300

    Lessons

--- lesson12/src/main/java/com/example/lesson12/MainActivity.java
+++ lesson12/src/main/java/com/example/lesson12/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.lesson12;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu m){
+        m.add("menu 1");
+        m.add("menu 2");
+        m.add("menu 3");
+        m.add("menu 4");
+        return super.onCreateOptionsMenu(m);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Toast.makeText(this,item.getTitle(),Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
